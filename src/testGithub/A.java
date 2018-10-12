d114b694150bfe9cf08150fd40217d95ba808a58
whyzc <35511503+whyzc@users.noreply.github.com>
2018-10-12 14:17:21 +0800

    test1

---
+++ src/testGithub/A.java
@@ -0,0 +1,11 @@
+package testGithub;
+
+public class A
+{
+	int a = 10;
+	
+	public int a(int n)
+	{
+		return a*n;
+	}
+}
---
+++ src/testGithub/B.java
@@ -0,0 +1,11 @@
+package testGithub;
+
+public class B
+{
+	public static void main(String[] args)
+	{
+		A a = new A();
+		int b = a.a(3);
+		System.out.println(b);
+	}
+}
